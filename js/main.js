f3be2c23b8fea0d6dbd46d314f706c6a65aee028
Macbit <antonpuzorjov@Antons-MacBook-Pro.local>
2017-07-14 17:33:56 +0100

    Fix top header margin in JS

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ $(document).ready(function(){
     });
 
     $("#clients-logo").owlCarousel({
- 
+
         itemsCustom : false,
         pagination : false,
         items : 5,
@@ -95,8 +95,8 @@ $(".fancybox").fancybox({
 
     closeClick : true,
     helpers : {
-        title : { 
-            type: 'inside' 
+        title : {
+            type: 'inside'
         },
         overlay : {
             css : {
@@ -108,11 +108,4 @@ $(".fancybox").fancybox({
 
 
 
-
-
-
- 
-
-
-
-
+$( ".full_size" ).first().css( "margin-top", "100px" );
